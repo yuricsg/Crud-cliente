0a7d5f22642a84e93264c15843b4926d0d0e9081
yuricsg <yuriicardo004@gmail.com>
2025-01-15 23:40:38 -0300

    Criação do banco H2 e seeding da base de dados

---
+++ src/test/java/com/yuricsg/crud/DesafioCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuricsg.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
